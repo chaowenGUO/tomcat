809dbdaa0525917f9af5bb840110d30b66ce09bf
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 11:57:23 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -10,6 +10,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
