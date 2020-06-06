3cc32a9a0e6626e6fafaf57c9d0f9658099f2745
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 18:25:14 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -16,7 +16,21 @@ public class Main
         config.setJdbcUrl("jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVICE_HOST") + ":" + System.getenv("POSTGRESQL_SERVICE_PORT") + "/sampledb");
         config.setUsername("postgresql");
         config.setPassword("postgresql");
-        final var ds = new HikariDataSource(config);
+        final var dataSource = new HikariDataSource(config);
+        try (final var connection = dataSource.getConnection())
+        {
+            final var statement = connection.createStatement();
+            statement.executeUpdate("create table if not exists productItem (image int primary key, description text not null, price money not null)");
+            statement.executeUpdate("insert into productItem values (9543, \"好先生同款墨镜孙红雷偏光男士太阳镜韩明星\", 97.50), (9532, \"陌森太阳眼镜男女2016偏光定制驾驶近视\", 518.70)");
+            final var resultSet = stmt.executeQuery("select * from productItem");
+            while (resultSet.next())
+            {
+                System.out.print(resultSet.getInt("image"));
+                System.out.print(resultSet.getString("description"));
+                System.out.print(resultSet.getBigDecimal("price"));
+                System.out.println();
+            }
+        }
         SpringApplication.run(Main.class, args);
     }
 }
