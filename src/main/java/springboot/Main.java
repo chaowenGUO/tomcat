5aefc3bec1c6a8058f1a978c72699b8ce6f21785
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 13:50:33 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -1,8 +1,10 @@
 package springboot;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @RestController
 @EnableAutoConfiguration
@@ -10,6 +12,11 @@ public class Main
 {
     public static void main(String[] args)
     {
+        final var config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://" + System.getenv(POSTGRESQL_SERVICE_HOST) + ":" + System.getenv(POSTGRESQL_SERVICE_PORT) + "/" + System.getenv(POSTGRESQL_DATABASE:postgres));
+        config.setUsername(System.getenv("POSTGRESQL_USER"));
+        config.setPassword(System.getenv("POSTGRESQL_PASSWORD"));
+        final var ds = new HikariDataSource(config);
         SpringApplication.run(Main.class, args);
     }
 }
