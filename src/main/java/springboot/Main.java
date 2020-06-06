a02e527a66acbd4e3f87b6554ab3358e58af1be8
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 14:12:56 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -13,7 +13,7 @@ public class Main
     public static void main(String[] args)
     {
         final HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVICE_HOST") + ":" + System.getenv("POSTGRESQL_SERVICE_PORT") + "/" + System.getenv("POSTGRESQL_DATABASE"));
+        config.setJdbcUrl("jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVICE_HOST") + ":" + System.getenv("POSTGRESQL_SERVICE_PORT") + "/sampledb");
         config.setUsername(System.getenv("POSTGRESQL_USER"));
         config.setPassword(System.getenv("POSTGRESQL_PASSWORD"));
         final HikariDataSource ds = new HikariDataSource(config);
