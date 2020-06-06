c84ae3714b644597257c0f6e3652b925513081ef
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 11:40:48 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -1,15 +1,11 @@
 package springboot;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@EnableAutoConfiguration
+@org.springframework.web.bind.annotation.RestController
+@org.springframework.boot.autoconfigure.EnableAutoConfiguration
 public class Main
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Example.class, args);
+        org.springframework.boot.SpringApplication.run(Example.class, args);
     }
 }
