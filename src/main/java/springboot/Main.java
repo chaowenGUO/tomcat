ab564fe96de6e8e5c92218c5bd80df7be8e577d3
chaowen guo <chaowen.guo1@gmail.com>
2020-06-06 11:38:34 -0500

    Update and rename SpringbootDemoApplication.java to Main.java

---
+++ src/main/java/springboot/Main.java
@@ -0,0 +1,15 @@
+package springboot;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Example.class, args);
+    }
+}
