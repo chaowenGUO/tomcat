10227a638d27b61344decb2ac164745a9c8f332d
chaowen guo <chaowen.guo1@gmail.com>
2020-06-07 00:51:49 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -13,7 +13,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @SpringBootApplication
 public class Main
 {
-    @PostMapping(value="/ajax", consumes=MediaType.APPLICATION_JSON)
+    @PostMapping("/ajax")
     String ajax(@RequestBody final java.util.Map<String, Object> body)
     {
         return body.get("name").toString() + "index";
