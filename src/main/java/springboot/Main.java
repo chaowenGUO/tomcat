eb5d797c42a75625a339916889e2c48d0a519bfb
chaowen guo <chaowen.guo1@gmail.com>
2020-06-07 17:02:22 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -17,7 +17,7 @@ public class Main
     String ajax(@RequestBody final String body) throws Exception
     {
         final var config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVICE_HOST") + ":" + System.getenv("POSTGRESQL_SERVICE_PORT") + "/sampledb");
+        config.setJdbcUrl(String.join("", "jdbc:postgresql://", System.getenv("POSTGRESQL_SERVICE_HOST"), ":", System.getenv("POSTGRESQL_SERVICE_PORT"), "/sampledb"));
         config.setUsername("postgresql");
         config.setPassword("postgresql"); 
         final var objectMapper = new ObjectMapper();
@@ -27,6 +27,7 @@ public class Main
             try (final var statement = connection.createStatement())
             {
                 statement.executeUpdate("create table if not exists productItem (image int primary key, description text not null, price money not null)");
+                System.out.println(statement.executeQuery("select count(*) from productItem").getInt(1));
                 statement.executeUpdate("insert into productItem values (9543, '好先生同款墨镜孙红雷偏光男士太阳镜韩明星', 97.50), (9532, '陌森太阳眼镜男女2016偏光定制驾驶近视', 518.70)");
                 try (final var resultSet = statement.executeQuery("select * from productItem"))
                 {                   
