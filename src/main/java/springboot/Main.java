a8c963bd371642cfcebe1023ec43061b2912044b
chaowen guo <chaowen.guo1@gmail.com>
2020-06-07 12:32:56 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -27,18 +27,22 @@ public class Main
         config.setPassword("postgresql"); 
         final var dataSource = new HikariDataSource(config);
         try (final var connection = dataSource.getConnection())
+        {
             try (final var statement = connection.createStatement())
             {
                 statement.executeUpdate("create table if not exists productItem (image int primary key, description int not null)");
                 statement.executeUpdate("insert into productItem values (9543, 1234), (9532, 5678)");
                 try (final var resultSet = statement.executeQuery("select * from productItem"))
+                {
                     while (resultSet.next())
                     {
                         System.out.print(resultSet.getInt("image"));
                         System.out.print(resultSet.getInt("description"));
                         System.out.println();
                     }
+                }
             }
+        }
         SpringApplication.run(Main.class, args);
     }
 }
