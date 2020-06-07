bb5a9210fd457753658b91e69d1d967c10578827
chaowen guo <chaowen.guo1@gmail.com>
2020-06-07 16:08:55 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -34,7 +34,7 @@ public class Main
                     {
                         final var metaData = resultSet.getMetaData();
                         final var objectNode = objectMapper.createObjectNode();
-                        for (var column = 1; column != metaData.getColumnCount() + 1; ++column) objectNode.putPOJO(metaData.getColumnName(column), resultSet.getObject(column)));
+                        for (var column = 1; column != metaData.getColumnCount() + 1; ++column) objectNode.putPOJO(metaData.getColumnName(column), resultSet.getObject(column));
                         arrayNode.add(objectNode)
                     }
                 }
