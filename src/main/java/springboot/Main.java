19ef2e63ed7dd070cd06a5d629f1454def70d347
chaowen guo <chaowen.guo1@gmail.com>
2020-06-10 14:30:01 -0500

    Update Main.java

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
     
 @RestController
 @SpringBootApplication
-public final class Main
+public class Main
 {
     @Component
     private static final class DataSource
