da69683d5933dac2c53b46f19aa1a90a947671df
chaowen guo <chaowen.guo1@gmail.com>
2020-06-18 19:25:46 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -3,7 +3,7 @@ with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
     repository.index.remove(['src/main/resources/static'], True, r=True)
-    repository.index.commit('') #git commit --allow-empty-message -m ''
+    repository.index.commit('eve') #git commit --allow-empty-message -m ''
     repository.git.subtree('add', '--prefix=src/main/resources/static', 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
             #cd src/main/resources/static
             #git rm -rf .github/workflows Procfile *.txt *.py README.md
