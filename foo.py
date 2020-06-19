998e221cd522193fb4b0d68bb267efefacc68ffa
chaowen guo <chaowen.guo1@gmail.com>
2020-06-18 19:09:56 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -1,12 +1,12 @@
 import git
 with git.Repo('.') as repository:
+    repository.config_writer().set_value('user', 'name', 'Your Name').release()
+    repository.config_writer().set_value('user', 'email', 'you@example.com').release()
     repository.index.remove(['src/main/resources/static'], True, r=True)
-#git config user.email 'you@example.com'
-#            git config user.name 'Your Name'
-#            git commit --allow-empty-message -m ''
-#            git subtree add --prefix=src/main/resources/static https://github.com/chaowenGUO/aiohttp master --squash
-#            cd src/main/resources/static
-#            git rm -rf .github/workflows Procfile *.txt *.py README.md
-#            git mv -f database.sql ..
-#            git commit --allow-empty-message -m ''
-#            git push
+    repository.index.commit() #git commit --allow-empty-message -m ''
+    #git subtree add --prefix=src/main/resources/static https://github.com/chaowenGUO/aiohttp master --squash
+            #cd src/main/resources/static
+            #git rm -rf .github/workflows Procfile *.txt *.py README.md
+            #git mv -f database.sql ..
+            #git commit --allow-empty-message -m ''
+            #git push
