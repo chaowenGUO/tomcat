c83d3e550b1b26e295cdfbd9770ca6f50abbdacd
chaowen guo <chaowen.guo1@gmail.com>
2020-06-20 05:40:26 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -6,7 +6,7 @@ with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.index.remove([static], True, r=True)
     repository.index.commit('') #git commit --allow-empty-message -m ''
     repository.git.subtree('add', '--prefix=' + static, 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
-    repository.index.move([static + 'database.sql', pathlib.Path(static).resolve().parent], f=True)
+    repository.index.move([static + 'database.sql', str(pathlib.Path(static).resolve().parent)], f=True)
     repository.index.remove([str(_) for _  in pathlib.Path(static).iterdir() if _.suffix != '.html' and _.suffix != '.js'], True, r=True)
     repository.index.commit('')
     repository.remote().push()
