819cf5deaa7203a19887fac9a557dcfe04a88295
chaowen guo <chaowen.guo1@gmail.com>
2020-06-20 04:34:38 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -5,6 +5,7 @@ with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.index.remove(['src/main/resources/static'], True, r=True)
     repository.index.commit('eve') #git commit --allow-empty-message -m ''
     repository.git.subtree('add', '--prefix=src/main/resources/static', 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
+    repository.index.move(['src/main/resources/static/database.sql', 'src/main/resources'])
     print([*pathlib.Path('src/main/resources/static').iterdir()])
             #cd src/main/resources/static
             #git rm -rf .github/workflows Procfile *.txt *.py README.md
