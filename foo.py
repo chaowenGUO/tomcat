e8921311356a33ab678462c41425b14e6cbc1f40
chaowen guo <chaowen.guo1@gmail.com>
2020-06-20 05:39:00 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -2,10 +2,11 @@ import git, pathlib
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
-    repository.index.remove(['src/main/resources/static'], True, r=True)
+    static = 'src/main/resources/static'
+    repository.index.remove([static], True, r=True)
     repository.index.commit('') #git commit --allow-empty-message -m ''
-    repository.git.subtree('add', '--prefix=src/main/resources/static', 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
-    repository.index.move(['src/main/resources/static/database.sql', 'src/main/resources'], f=True)
-    repository.index.remove([str(_) for _  in pathlib.Path('src/main/resources/static').iterdir() if _.suffix != '.html' and _.suffix != '.js'], True, r=True)
-    #repository.index.commit('')
+    repository.git.subtree('add', '--prefix=' + static, 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
+    repository.index.move([static + 'database.sql', pathlib.Path(static).resolve().parent], f=True)
+    repository.index.remove([str(_) for _  in pathlib.Path(static).iterdir() if _.suffix != '.html' and _.suffix != '.js'], True, r=True)
+    repository.index.commit('')
     repository.remote().push()
