3ce2c3a1b7659d0aca158c273f4189b0a991cc12
Your Name <you@example.com>
2020-07-06 08:30:35 +0000

    Squashed 'src/main/resources/static/' content from commit 18d64fe
    git-subtree-dir: src/main/resources/static
    git-subtree-split: 18d64fe8aa674b13450002da06937a7ce6bdf95b

---
+++ foo.py
@@ -0,0 +1,6 @@
+import aiohttp, asyncio, json, os
+async def f():
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://api.github.com/repos/chaowenGUO/springboot/dispatches', data=json.dumps({'event_type':'ping'}).encode(), auth=aiohttp.BasicAuth('chaowenGUO', os.getenv('TOKEN'))) as _: pass
+
+asyncio.run(f())
