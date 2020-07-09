cf967379853ee267d9000720b2e58747e666ec8d
chaowen guo <chaowen.guo1@gmail.com>
2020-07-09 11:13:11 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -36,7 +36,7 @@ public class Main
             //config.setUsername("postgresql");
             //config.setPassword("postgresql");
             final var config = new HikariConfig();
-            //final var dbUri = new java.net.URI(System.getenv("DATABASE_URL"));
+            final var dbUri = new java.net.URI(System.getenv("DATABASE_URL"));
             //config.setJdbcUrl(String.join("", "jdbc:postgresql://", dbUri.getHost(), ":", dbUri.getPort(), dbUri.getPath()));
             //config.setUsername(dbUri.getUserInfo().split(":")[0]);
             //config.setPassword(dbUri.getUserInfo().split(":")[1]);
