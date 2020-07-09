f0d23920db3dd95a72d5ec16692f7b69ae96ab98
chaowen guo <chaowen.guo1@gmail.com>
2020-07-09 11:10:41 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -36,10 +36,11 @@ public class Main
             //config.setUsername("postgresql");
             //config.setPassword("postgresql");
             final var config = new HikariConfig();
-            final var dbUri = new java.net.URI(System.getenv("DATABASE_URL"));
-            config.setJdbcUrl(String.join("", "jdbc:postgresql://", dbUri.getHost(), ":", dbUri.getPort(), dbUri.getPath()));
-            config.setUsername(dbUri.getUserInfo().split(":")[0]);
-            config.setPassword(dbUri.getUserInfo().split(":")[1]);
+            //final var dbUri = new java.net.URI(System.getenv("DATABASE_URL"));
+            //config.setJdbcUrl(String.join("", "jdbc:postgresql://", dbUri.getHost(), ":", dbUri.getPort(), dbUri.getPath()));
+            //config.setUsername(dbUri.getUserInfo().split(":")[0]);
+            //config.setPassword(dbUri.getUserInfo().split(":")[1]);
+            config.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
             this.dataSource = new HikariDataSource(config);
             try (final var connection = this.dataSource.getConnection())
             {
