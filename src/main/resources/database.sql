52fb9dd357ec8984ba0d42b3e1a4222a4b6e6dff
chaowen guo <chaowen.guo1@gmail.com>
2020-07-12 10:58:41 -0500

    Update database.sql

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,5 +1,4 @@
-CREATE TABLE EMPLOYEE_SALARY
-     (DEPTNO   CHAR(3)      NOT NULL,
-      DEPTNAME VARCHAR(36)  NOT NULL,
-      EMPNO    CHAR(6)      NOT NULL,
-      SALARY   DECIMAL(9,2) NOT NULL WITH DEFAULT)
+create table EMPLOYEE_SALARY (DEPTNO   CHAR(3)      NOT NULL,
+                              DEPTNAME VARCHAR(36)  NOT NULL,
+                              EMPNO    CHAR(6)      NOT NULL,
+                              ALARY   DECIMAL(9,2) NOT NULL WITH DEFAULT);
