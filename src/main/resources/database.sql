1498ef1ae30e01ae7ef5c9208491ddfdc4174f00
chaowen guo <chaowen.guo1@gmail.com>
2020-07-12 11:18:38 -0500

    Update database.sql

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,4 +1,4 @@
 create table EMPLOYEE_SALARY (DEPTNO   CHAR(3)      NOT NULL,
                               DEPTNAME VARCHAR(36)  NOT NULL,
                               EMPNO    CHAR(6)      NOT NULL,
-                              ALARY   DECIMAL(9,2) NOT NULL WITH DEFAULT);
+                              ALARY   DECIMAL(9,2) NOT NULL WITH DEFAULT)
