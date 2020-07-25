774f21d18c0f505017146bff78e0dd418bcb67d7
chaowen guo <chaowen.guo1@gmail.com>
2020-07-24 21:55:03 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,9 +1,10 @@
-import git, pathlib
+import git, pathlib, shutil
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
     static = 'src/main/resources/static'
     repository.git.subtree('add', '--prefix=' + static, 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
-    repository.index.move([static + '/database.sql', str(pathlib.Path(static).resolve().parent)], f=True)
+    repository.index.move([static + '/database.sql', str(pathlib.Path(static).resolve().parent)], f=True)    
     for _  in pathlib.Path(static).iterdir():
-        if _.suffix != '.html' and _.suffix != '.js': _.unlink()
+        if _.suffix != '.html' and _.suffix != '.js' and _.is_file(): _.unlink()
+        else if _.is_dir(): shutil.rmtree(_)
