557fc9eab0b1bfe7b46e67ad558452e5bc967977
chaowen guo <chaowen.guo1@gmail.com>
2020-07-24 22:23:18 -0500

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import git, pathlib, shutil
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
-    static = pathlib.Path('src/main/resources/static').resolve()
+    static = pathlib.Path('src/main/resources/static')
     repository.git.subtree('add', '--prefix=' + str(static), 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
     shutil.move(str(static / 'database.sql'), static.parent)
     for _  in static.iterdir():
