e9d37bdc16355b1ab86dcff1e3033cdc110d30a5
chaowen guo <chaowen.guo1@gmail.com>
2020-09-22 07:38:47 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -158,7 +158,7 @@ public class Main
         {
             return ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
         }
-        catch (Exception error){return Mono.enpty();}
+        catch (Exception error){return Mono.empty();}
     }
     @Bean
     public RouterFunction<ServerResponse> route()
