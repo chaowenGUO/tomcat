9992aceb62a0720db97d3c0ad0873908ee680092
chaowen guo <chaowen.guo1@gmail.com>
2020-09-22 02:46:21 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import org.springframework.web.bind.annotation.PostMapping;
+/*import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -130,4 +130,63 @@ public class Main
         app.setDefaultProperties(java.util.Collections.singletonMap("server.port", System.getenv("PORT")));
         app.run(args);
     }
+}*/
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import reactor.ipc.netty.http.server.HttpServer;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
+import static org.springframework.web.reactive.function.server.RequestPredicates.method;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
+
+public class Server {
+
+	public static final String HOST = "localhost";
+
+	public static final int PORT = 8080;
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server();
+		server.startReactorServer();
+//		server.startTomcatServer();
+
+		System.out.println("Press ENTER to exit.");
+		System.in.read();
+	}
+
+	public RouterFunction<ServerResponse> routingFunction() {
+		PersonRepository repository = new DummyPersonRepository();
+		PersonHandler handler = new PersonHandler(repository);
+
+		return nest(path("/person"),
+				nest(accept(APPLICATION_JSON),
+						route(GET("/{id}"), handler::getPerson)
+						.andRoute(method(HttpMethod.GET), handler::listPeople)
+				).andRoute(POST("/").and(contentType(APPLICATION_JSON)), handler::createPerson));
+	}
+
+	public void startReactorServer() throws InterruptedException {
+		RouterFunction<ServerResponse> route = routingFunction();
+		HttpHandler httpHandler = toHttpHandler(route);
+
+		ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
+		HttpServer server = HttpServer.create(HOST, PORT);
+		server.newHandler(adapter).block();
+	}
 }
