f1c48b222b30409ee318edfcb311dd674aa22bcd
chaowen guo <chaowen.guo1@gmail.com>
2020-09-24 11:58:30 -0500

    Update build.py

--- build.py
+++ build.py
@@ -6,5 +6,4 @@ with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.git.subtree('add', '--prefix=' + str(static), 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
     static = static.resolve()
     for _  in static.iterdir():
-        if _.suffix == '.html' or _.suffix == '.js' or _.suffix == '.sql': shutil.move(str(_), pathlib.Path('src/main/static'))
-    shutil.rmtree(static)
+        if _.suffix != '.html' and _.suffix != '.js' and _.suffix != '.sql': _.unlink()
