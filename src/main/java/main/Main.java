164346cd0bbc54f4b7cecb5e5bb542e902f0c529
chaowen guo <chaowen.guo1@gmail.com>
2020-09-24 03:00:13 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -147,7 +147,7 @@ import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
 
-public class Application {
+public class Main {
 
     public static void main(String[] args) {
         DisposableServer server =
