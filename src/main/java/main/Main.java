5df47b4980fd9a7aee05609aff639e22f6cf85a0
chaowen guo <chaowen.guo1@gmail.com>
2020-09-24 05:05:57 -0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -149,7 +149,7 @@ public class Main
 {
     public static void main(final String[] args) throws Exception
     {
-	final var resource = java.nio.file.Paths.get(Main.class.getResource("/static").toURI());
+	final var resource = java.nio.file.Paths.get(Main.class.getResource("static").toURI());
         final var server = reactor.netty.http.server.HttpServer.create().port(Integer.parseInt(System.getenv("port"))).route(
 		routes -> routes.directory("/", resource)).bindNow();
         server.onDispose().block();
