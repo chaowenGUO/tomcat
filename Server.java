71f37a29aecb465c454985584de7ad3f357fb294
chaowen guo <chaowen.guo1@gmail.com>
2020-10-02 17:46:08 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -148,15 +148,15 @@ public class Server
         final var vertx = io.vertx.core.Vertx.vertx();
         final var router = io.vertx.ext.web.Router.router(vertx);
         router.route("/").handler(request -> request.response().sendFile("login.html"));
-        router.route("/*").handler(io.vertx.ext.web.handler.StaticHandler.create("."));
         final var client = io.vertx.pgclient.PgPool.pool(vertx, System.getenv("DATABASE_URL").replace("postgres", "postgresql"));
         client.query(vertx.fileSystem().readFileBlocking("database.sql").toString()).execute().toCompletionStage().toCompletableFuture().join();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> client.query("drop table productItem, productUnit, productReview").execute().toCompletionStage().toCompletableFuture().join()));
-        /*router.route("/ajax").handler(request -> {
+        router.route("/ajax").handler(request -> {
             final var json = new io.vertx.core.json.JsonArray();
             for (final var $: client.query("select * from productItem").execute().toCompletionStage().toCompletableFuture().join()) json.add($.toJson());
             request.json(json);
-        });*/
+        });
+        router.route("/*").handler(io.vertx.ext.web.handler.StaticHandler.create("."));
         vertx.createHttpServer().requestHandler(router).listen(Integer.parseInt(System.getenv("PORT")));
     }
 }
