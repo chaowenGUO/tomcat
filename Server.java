9285b0e86f751904877cb5eefc3f5d5915a51dcb
chaowen guo <chaowen.guo1@gmail.com>
2020-10-02 08:46:07 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -150,7 +150,7 @@ public class Server
         router.route("/").handler(request -> request.response().sendFile("login.html"));
         router.route("/*").handler(io.vertx.ext.web.handler.StaticHandler.create("."));
         final var client = io.vertx.pgclient.PgPool.pool(vertx, System.getenv("DATABASE_URL").replace("postgres", "postgresql"));
-        client.query(vertx.fileSystem().readFile("database.sql")).execute();
+        client.query(vertx.fileSystem().readFileBlocking("database.sql").toString()).execute();
         //client.query("select * from productItem").execute(ar -> {
   //if (ar.succeeded()) {
   //  System.out.println("Got " + ar.result() + " rows ");
