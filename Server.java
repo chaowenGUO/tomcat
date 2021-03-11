0ae5a09f2fcf242b1765875b2ff595fcef827c76
chaowen guo <chaowen.guo1@gmail.com>
2021-03-10 20:35:17 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -163,3 +163,30 @@ public class Server
         vertx.createHttpServer().requestHandler(router).listen(Integer.parseInt(System.getenv("PORT")));
     }
 }
+
+/*import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+async def database(app):
+    app.setdefault('database', await asyncpg.create_pool(host='postgres', user='postgres', database='postgres', password='postgres'))
+    app.setdefault('cache', aredis.StrictRedis('redis').cache('cache'))
+    #producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka')
+    #await producer.start()
+    #await producer.send_and_wait('topic', b"Super message")
+    #await producer.stop()
+    yield
+    await app.get('database').close()
+
+async def post(request):
+    body = await request.json()
+    body = ' '.join(' '.join((key, str(value))) for key,value in body.items())
+    records = await request.app.get('cache').get(body)
+    if not records:
+        async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=str)
+        await request.app.get('cache').set(body, records)
+    return aiohttp.web.Response(text=records)
+
+app = aiohttp.web.Application()
+app.add_routes([aiohttp.web.post('/ajax', post)])
+app.cleanup_ctx.append(database)
+aiohttp.web.run_app(app)*/
